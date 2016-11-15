244c8f42c5bd1a037a7b4b1c3403a5757958edbf
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-11-15 14:45:53 +0100

    image are resized, populate data moved to class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import com.codecool.shop.controller.ProductCategoryController;
 import com.codecool.shop.controller.ProductController;
 import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.*;
+import com.codecool.shop.exampleData.ExampleData;
 import com.codecool.shop.factory.ProductFactory;
 import com.codecool.shop.model.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
@@ -19,7 +20,7 @@ public class Main {
         port(8888);
 
         // populate some data for the memory storage
-        populateData();
+        ExampleData.populateDate();
 
         // Always start with more specific routes
         get("/hello", (req, res) -> "Hello World");
@@ -33,25 +34,7 @@ public class Main {
         enableDebugScreen();
     }
 
-    public static void populateData() {
 
-        //setting up a new supplier
-        Supplier amazon = ProductFactory.getInstance().supplier("Amazon", "Digital content and services");
-        Supplier lenovo = ProductFactory.getInstance().supplier("Lenovo", "Computers");
-        Supplier codecool = ProductFactory.getInstance().supplier("Codecool", "Course");
-
-        //setting up a new product category
-        ProductCategory tablet = ProductFactory.getInstance().productCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        ProductCategory laptop = ProductFactory.getInstance().productCategory("Laptop", "Hardware", " A laptop computer");
-        ProductCategory course = ProductFactory.getInstance().productCategory("Course", "Education", "Learning materials");
-
-        //setting up products and printing it
-        ProductFactory.getInstance().product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
-        ProductFactory.getInstance().product("Lenovo IdeaPad Miix 700", 479f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
-        ProductFactory.getInstance().product("Amazon Fire HD 8", 89f, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
-        ProductFactory.getInstance().product("Lenovo YogaBook 530", 2000f, "USD", "Yogabook", laptop, lenovo);
-        ProductFactory.getInstance().product("Codecool Programming Course", 543f, "USD", "From base to advanced", course, codecool);
-    }
 
 
 }
---
+++ src/main/java/com/codecool/shop/exampleData/ExampleData.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.exampleData;
+
+
+import com.codecool.shop.factory.ProductFactory;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+public class ExampleData {
+
+    public static  void populateDate(){
+        Supplier amazon = ProductFactory.getInstance().supplier("Amazon", "Digital content and services");
+        Supplier lenovo = ProductFactory.getInstance().supplier("Lenovo", "Computers");
+        Supplier codecool = ProductFactory.getInstance().supplier("Codecool", "Course");
+
+        //setting up a new product category
+        ProductCategory tablet = ProductFactory.getInstance().productCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory laptop = ProductFactory.getInstance().productCategory("Laptop", "Hardware", " A laptop computer");
+        ProductCategory course = ProductFactory.getInstance().productCategory("Course", "Education", "Learning materials");
+
+        //setting up products and printing it
+        ProductFactory.getInstance().product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        ProductFactory.getInstance().product("Lenovo IdeaPad Miix 700", 479f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
+        ProductFactory.getInstance().product("Amazon Fire HD 8", 89f, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
+        ProductFactory.getInstance().product("Lenovo YogaBook 530", 2000f, "USD", "Yogabook", laptop, lenovo);
+        ProductFactory.getInstance().product("Codecool Programming Course", 543f, "USD", "From base to advanced", course, codecool);
+    }
+}
