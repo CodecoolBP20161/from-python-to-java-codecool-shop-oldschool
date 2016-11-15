8061e87eca55208ab8847f9d266c24dbdda4fb40
eszterkaloz <eszterkaloz@gmail.com>
2016-11-15 14:12:36 +0100

    Implement controller class for rendering products by categories

---
+++ src/main/java/com/codecool/shop/controller/ProductCategoryController.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+
+import spark.Request;
+import spark.Response;
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductCategoryController {
+    public static ModelAndView renderProductsByCategory(Request req, Response res) {
+        ProductDaoMem productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDaoMem productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+        Map params = new HashMap<>();
+        params.put("products", productDataStore.getBy(productCategoryDataStore.find(Integer.parseInt(req.params(":id")))));
+        params.put("categories", productCategoryDataStore.getAll());
+        return new ModelAndView(params, "product/index");
+    }
+}
