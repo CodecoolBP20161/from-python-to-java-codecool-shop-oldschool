a2584945c1ac45b08a58e6821c158d18b0ee2278
eszterkaloz <eszterkaloz@gmail.com>
2016-11-15 15:32:32 +0100

    Implemented SupplierController class

---
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -0,0 +1,25 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+
+import spark.Request;
+import spark.Response;
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SupplierController {
+
+    public static ModelAndView renderProductsBySupplier(Request req, Response res) {
+        ProductDaoMem productDataStore = ProductDaoMem.getInstance();
+        SupplierDaoMem supplierDataStore = SupplierDaoMem.getInstance();
+
+        Map params = new HashMap<>();
+        params.put("products", productDataStore.getBy(supplierDataStore.find(Integer.parseInt(req.params(":id")))));
+        params.put("suppliers", supplierDataStore.getAll());
+        return new ModelAndView(params, "product/index");
+    }
+
+}
