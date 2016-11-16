860202b9b62eb6cf73af9ef432e1dd53389cd36e
Kristof Galla <gallakristof@gmail.com>
2016-11-16 16:20:27 +0100

    Add getNumberOfLineItemsInCart method to Order

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -4,36 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order implements Orderable {
-    private List lineItems = new ArrayList();
+    private List<LineItem> lineItems = new ArrayList();
 
     public List getLineItems() {
         return lineItems;
     }
 
-    public Order(){
+    public Order() {
 
     }
 
     public void addProduct(Product product) {
         LineItem newItem = new LineItem(product);
         LineItem chosenItem;
-        if (lineItems.contains(newItem)){
-           chosenItem = (LineItem)lineItems.get(lineItems.indexOf(newItem));
-           chosenItem.setQuantity(chosenItem.getQuantity() + 1);
-        }
-        else {
+        if (lineItems.contains(newItem)) {
+            chosenItem = lineItems.get(lineItems.indexOf(newItem));
+            chosenItem.setQuantity(chosenItem.getQuantity() + 1);
+        } else {
             lineItems.add(newItem);
         }
         System.out.println("lineItems = " + lineItems);
     }
 
-    public long sumLineItem(){
-        long result = 0;
+    public int sumLineItem() {
+        int result = 0;
         for (int i = 0; i < lineItems.size(); i++) {
-            LineItem lineItem = (LineItem) lineItems.get(i);
-            result += (long)lineItem.getUnitPrice();
+            result += lineItems.get(i).getUnitPrice() * lineItems.get(i).getQuantity();
         }
 
         return result;
+    }
+
+    public int getNumberOfLineItemsInCart() {
+        int sum = 0;
+        for (int i = 0; i < lineItems.size(); i++) {
+            sum += lineItems.get(i).getQuantity();
+        }
+        return sum;
     }
 }
