f8de0964c5fef1a51aa2e0a5ef1b77317be9293b
eszterkaloz <eszterkaloz@gmail.com>
2016-11-16 14:38:22 +0100

    Route for adding products to cart

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -9,18 +9,14 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-public class OrderController extends ShopController{
-
+public class OrderController extends ShopController {
 
     public static ModelAndView renderOrder(Request req, Response res) {
-
-        System.out.println("OrderControll");
         Orderable order = new Order();
-        req.queryParams(":product-id");
+        req.params(":product-id");
 
-        Product orderedProduct = productDataStore.find(Integer.parseInt(req.queryParams(":product-id")));
+        Product orderedProduct = productDataStore.find(Integer.parseInt(req.params(":product-id")));
         order.addProduct(orderedProduct);
-        System.out.println("orderedProduct = " + orderedProduct);
 
 //        if (req.session().attribute("order") != null){
 //
