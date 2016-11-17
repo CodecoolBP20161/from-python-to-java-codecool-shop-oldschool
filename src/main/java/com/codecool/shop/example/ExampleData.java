6bac46a6a289c5836b080a8b0fbce58180dfb89b
eszterkaloz <eszterkaloz@gmail.com>
2016-11-17 10:05:27 +0100

    Longer description added

--- src/main/java/com/codecool/shop/example/ExampleData.java
+++ src/main/java/com/codecool/shop/example/ExampleData.java
@@ -20,7 +20,7 @@ public class ExampleData {
         ProductFactory.getInstance().product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
         ProductFactory.getInstance().product("Lenovo IdeaPad Miix 700", 479f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
         ProductFactory.getInstance().product("Amazon Fire HD 8", 89f, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
-        ProductFactory.getInstance().product("Lenovo YogaBook 530", 2000f, "USD", "Yogabook", laptop, lenovo);
-        ProductFactory.getInstance().product("Codecool Programming Course", 543f, "USD", "From base to advanced", course, codecool);
+        ProductFactory.getInstance().product("Lenovo YogaBook 530", 499.9f, "USD", "The affordable & light Yoga 500 is a multimode laptop with fast, cutting-edge processing power.", laptop, lenovo);
+        ProductFactory.getInstance().product("Codecool Programming Course", 2000f, "USD", "Codecool - Where we train programmers with project experience in up to 18 months.", course, codecool);
     }
 }
