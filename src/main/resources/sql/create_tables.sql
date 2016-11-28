222773ef43fa2be1234098c779f1455a75bb439a
eszterkaloz <eszterkaloz@gmail.com>
2016-11-28 14:50:49 +0100

    Changed ID fields to int

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS suppliers;
 
 CREATE TABLE product_categories
 (
-  id          VARCHAR(36) PRIMARY KEY,
+  id          INT PRIMARY KEY,
   name        VARCHAR(40),
   department  VARCHAR(10),
   description VARCHAR(500)
@@ -13,17 +13,17 @@ CREATE TABLE product_categories
 
 CREATE TABLE suppliers
 (
-  id   VARCHAR(36) PRIMARY KEY,
+  id   INT PRIMARY KEY,
   name VARCHAR(40)
 );
 
 CREATE TABLE products
 (
-  id               VARCHAR(36) PRIMARY KEY,
+  id               INT PRIMARY KEY,
   name             VARCHAR(40),
   description      VARCHAR(500),
   default_price    VARCHAR(10),
   default_currency VARCHAR(10),
-  product_category VARCHAR(10) REFERENCES product_categories (id),
-  supplier         VARCHAR(10) REFERENCES suppliers (id)
+  product_category INT REFERENCES product_categories (id),
+  supplier         INT REFERENCES suppliers (id)
 );
