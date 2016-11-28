5361d66f9cd0118f5c05367f16cce1a38df3c170
Kristof Galla <gallakristof@gmail.com>
2016-11-28 14:17:27 +0100

    Add SQL script for table creation in existing database

---
+++ src/main/resources/sql/create_tables.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS suppliers;
+
+
+CREATE TABLE product_categories
+(
+  id          VARCHAR(36) PRIMARY KEY,
+  name        VARCHAR(40),
+  department  VARCHAR(10),
+  description VARCHAR(10)
+);
+
+CREATE TABLE suppliers
+(
+  id   VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(40)
+);
+
+CREATE TABLE products
+(
+  id               VARCHAR(36) PRIMARY KEY,
+  name             VARCHAR(40),
+  description      VARCHAR(500),
+  default_price    VARCHAR(10),
+  default_currency VARCHAR(10),
+  product_category VARCHAR(10) REFERENCES product_categories (id),
+  supplier         VARCHAR(10) REFERENCES suppliers (id)
+);
