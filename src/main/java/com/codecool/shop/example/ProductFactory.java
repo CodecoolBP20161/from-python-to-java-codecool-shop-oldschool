d61bc5744216b07e063796d64415abb27be0cb77
eszterkaloz <eszterkaloz@gmail.com>
2016-11-29 11:12:05 +0100

    Changed example factory to use datastoragefactory methods

--- src/main/java/com/codecool/shop/example/ProductFactory.java
+++ src/main/java/com/codecool/shop/example/ProductFactory.java
@@ -1,11 +1,13 @@
 package com.codecool.shop.example;
 
+import com.codecool.shop.dao.DataStorageFactory;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.database.ProductDaoJDBC;
+import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
+import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -23,19 +25,19 @@ public class ProductFactory {
     }
 
     public void product(String name, float defaultPrice, String defaultCurrency, String description, ProductCategory productCategory, Supplier supplier) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductDao productDataStore = DataStorageFactory.productDaoFactory();
         productDataStore.add(new Product(name, defaultPrice, defaultCurrency, description, productCategory, supplier));
     }
 
     public ProductCategory productCategory(String name, String department, String description) {
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = DataStorageFactory.productCategoryDaoFactory();
         ProductCategory result = new ProductCategory(name, department, description);
         productCategoryDataStore.add(result);
         return result;
     }
 
     public Supplier supplier(String name, String description) {
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        SupplierDao supplierDataStore = DataStorageFactory.supplierDaoFactory();
         Supplier result = new Supplier(name, description);
         supplierDataStore.add(result);
         return result;
