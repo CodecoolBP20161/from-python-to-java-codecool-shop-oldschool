1d01d9291a4135141305446eef9bc31c03984320
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-11-29 10:56:39 +0100

    datastorage and datastoragefactory are ready

---
+++ src/main/java/com/codecool/shop/dao/DataStorageFactory.java
@@ -0,0 +1,59 @@
+package com.codecool.shop.dao;
+
+
+import com.codecool.shop.dao.implementation.database.ProductDaoJDBC;
+import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
+import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
+
+
+public class DataStorageFactory {
+
+    private static DataStorage dataStorage = DataStorage.MEMORY;
+
+    public static ProductDao productDaoFactory(){
+        ProductDao result;
+        switch (dataStorage) {
+            case MEMORY:
+                result = ProductDaoMem.getInstance();
+                break;
+            case DATABASE:
+                result = new ProductDaoJDBC();
+                break;
+            default:
+                result = null;
+        }
+        return result;
+    }
+
+    public static ProductCategoryDao productCategoryDaoFactory(){
+        ProductCategoryDao result;
+        switch (dataStorage) {
+            case MEMORY:
+                result = ProductCategoryDaoMem.getInstance();
+                break;
+            case DATABASE:
+                result = null; //new ProductCategoryDaoJDBC();
+                break;
+            default:
+                result = null;
+        }
+        return result;
+    }
+
+    public static SupplierDao supplierDaoFactory(){
+        SupplierDao result;
+        switch (dataStorage) {
+            case MEMORY:
+                result = SupplierDaoMem.getInstance();
+                break;
+            case DATABASE:
+                result = null; //new SupplierDaoJDBC();
+                break;
+            default:
+                result = null;
+        }
+        return result;
+    }
+
+}
