83ddfca050c4ea34d73f5b7f39f524dcd262057e
eszterkaloz <eszterkaloz@gmail.com>
2016-11-29 11:35:58 +0100

    Datastorage changes applied

--- src/main/java/com/codecool/shop/controller/ShopController.java
+++ src/main/java/com/codecool/shop/controller/ShopController.java
@@ -1,5 +1,9 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.DataStorageFactory;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
 import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
@@ -14,10 +18,10 @@ import java.util.Map;
 
 public abstract class ShopController {
 
-    //get singleton instance to handle data in memory
-    protected static ProductDaoMem productDataStore = ProductDaoMem.getInstance();
-    protected static ProductCategoryDaoMem productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-    protected static SupplierDaoMem supplierDataStore = SupplierDaoMem.getInstance();
+    //
+    protected static ProductDao productDataStore = DataStorageFactory.productDaoFactory();
+    protected static ProductCategoryDao productCategoryDataStore = DataStorageFactory.productCategoryDaoFactory();
+    protected static SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
 
     public static ModelAndView render(Request req, Response res) {
