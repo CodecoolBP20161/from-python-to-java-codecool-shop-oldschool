8402db4f753bd97683decfec61fbed65d8495c19
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-11-29 11:33:01 +0100

    example data with sql

--- src/main/java/com/codecool/shop/dao/DataStorageFactory.java
+++ src/main/java/com/codecool/shop/dao/DataStorageFactory.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao;
 
 
+import com.codecool.shop.dao.implementation.database.ProductCategoryDaoJdbc;
 import com.codecool.shop.dao.implementation.database.ProductDaoJDBC;
 import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
@@ -33,7 +34,7 @@ public class DataStorageFactory {
                 result = ProductCategoryDaoMem.getInstance();
                 break;
             case DATABASE:
-                result = null; //new ProductCategoryDaoJDBC();
+                result = new ProductCategoryDaoJdbc();
                 break;
             default:
                 result = null;
