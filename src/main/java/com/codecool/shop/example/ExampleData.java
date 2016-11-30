fa1cb2160ad0d814e4ec7d9f294ce6663531a6e6
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-11-30 11:26:26 +0100

    ids for exampledata

--- src/main/java/com/codecool/shop/example/ExampleData.java
+++ src/main/java/com/codecool/shop/example/ExampleData.java
@@ -7,20 +7,20 @@ import com.codecool.shop.model.Supplier;
 public class ExampleData {
 
     public static void populateData() {
-        Supplier amazon = ProductFactory.getInstance().supplier("Amazon", "Digital content and services");
-        Supplier lenovo = ProductFactory.getInstance().supplier("Lenovo", "Computers");
-        Supplier codecool = ProductFactory.getInstance().supplier("Codecool", "Course");
+        Supplier amazon = ProductFactory.getInstance().supplier(1, "Amazon", "Digital content and services");
+        Supplier lenovo = ProductFactory.getInstance().supplier(2, "Lenovo", "Computers");
+        Supplier codecool = ProductFactory.getInstance().supplier(3, "Codecool", "Course");
 
         //setting up a new product category
-        ProductCategory tablet = ProductFactory.getInstance().productCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        ProductCategory laptop = ProductFactory.getInstance().productCategory("Laptop", "Hardware", " A laptop computer");
-        ProductCategory course = ProductFactory.getInstance().productCategory("Course", "Education", "Learning materials");
+        ProductCategory tablet = ProductFactory.getInstance().productCategory(1, "Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory laptop = ProductFactory.getInstance().productCategory(2, "Laptop", "Hardware", " A laptop computer");
+        ProductCategory course = ProductFactory.getInstance().productCategory(3, "Course", "Education", "Learning materials");
 
         //setting up products and printing it
-        ProductFactory.getInstance().product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
-        ProductFactory.getInstance().product("Lenovo IdeaPad Miix 700", 479f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
-        ProductFactory.getInstance().product("Amazon Fire HD 8", 89f, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
-        ProductFactory.getInstance().product("Lenovo YogaBook 530", 499.9f, "USD", "The affordable & light Yoga 500 is a multimode laptop with fast, cutting-edge processing power.", laptop, lenovo);
-        ProductFactory.getInstance().product("Codecool Programming Course", 2000f, "USD", "Codecool - Where we train programmers with project experience in up to 18 months.", course, codecool);
+        ProductFactory.getInstance().product(1, "Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        ProductFactory.getInstance().product(2, "Lenovo IdeaPad Miix 700", 479f, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo);
+        ProductFactory.getInstance().product(3, "Amazon Fire HD 8", 89f, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
+        ProductFactory.getInstance().product(4, "Lenovo YogaBook 530", 499.9f, "USD", "The affordable & light Yoga 500 is a multimode laptop with fast, cutting-edge processing power.", laptop, lenovo);
+        ProductFactory.getInstance().product(5, "Codecool Programming Course", 2000f, "USD", "Codecool - Where we train programmers with project experience in up to 18 months.", course, codecool);
     }
 }
--- src/main/java/com/codecool/shop/example/ProductFactory.java
+++ src/main/java/com/codecool/shop/example/ProductFactory.java
@@ -1,13 +1,10 @@
 package com.codecool.shop.example;
 
+
 import com.codecool.shop.dao.DataStorageFactory;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.database.ProductDaoJDBC;
-import com.codecool.shop.dao.implementation.memory.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.memory.ProductDaoMem;
-import com.codecool.shop.dao.implementation.memory.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -24,22 +21,22 @@ public class ProductFactory {
         return instance;
     }
 
-    public ProductCategory productCategory(String name, String department, String description) {
+    public ProductCategory productCategory(int id, String name, String department, String description) {
         ProductCategoryDao productCategoryDataStore = DataStorageFactory.productCategoryDaoFactory();
-        ProductCategory result = new ProductCategory(name, department, description);
+        ProductCategory result = new ProductCategory(id, name, department, description);
         productCategoryDataStore.add(result);
         return result;
     }
 
-    public Supplier supplier(String name, String description) {
+    public Supplier supplier(int id, String name, String description) {
         SupplierDao supplierDataStore = DataStorageFactory.supplierDaoFactory();
-        Supplier result = new Supplier(name, description);
+        Supplier result = new Supplier(id, name, description);
         supplierDataStore.add(result);
         return result;
     }
 
-    public void product(String name, float defaultPrice, String defaultCurrency, String description, ProductCategory productCategory, Supplier supplier) {
+    public void product(int id, String name, float defaultPrice, String defaultCurrency, String description, ProductCategory productCategory, Supplier supplier) {
         ProductDao productDataStore = DataStorageFactory.productDaoFactory();
-        productDataStore.add(new Product(name, defaultPrice, defaultCurrency, description, productCategory, supplier));
+        productDataStore.add(new Product(id, name, defaultPrice, defaultCurrency, description, productCategory, supplier));
     }
 }
