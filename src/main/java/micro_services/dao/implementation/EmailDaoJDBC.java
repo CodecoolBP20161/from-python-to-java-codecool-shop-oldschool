9f26a9e4b1eaaad9444c8a52eff0ed0477254b3f
eszterkaloz <eszterkaloz@gmail.com>
2016-12-13 15:25:33 +0100

    Email dao implemented for JDBC

--- src/main/java/micro_services/dao/implementation/EmailDaoJDBC.java
+++ src/main/java/micro_services/dao/implementation/EmailDaoJDBC.java
@@ -1,5 +1,13 @@
 package micro_services.dao.implementation;
 
 
-public class EmailDaoJDBC {
+import micro_services.dao.EmailDao;
+import micro_services.model.Email;
+
+public class EmailDaoJDBC implements EmailDao {
+
+    @Override
+    public void add(Email email) {
+        // TODO implement this
+    }
 }
