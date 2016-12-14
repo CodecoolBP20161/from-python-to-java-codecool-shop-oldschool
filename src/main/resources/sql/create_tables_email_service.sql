bc47d62024f87b60e5dc1d88208e7cd6eaee32c4
eszterkaloz <eszterkaloz@gmail.com>
2016-12-14 11:37:26 +0100

    Changed email ID to serial

--- src/main/resources/sql/create_tables_email_service.sql
+++ src/main/resources/sql/create_tables_email_service.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS emails;
 
 CREATE TABLE emails
 (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   to_address VARCHAR(50),
   password VARCHAR(50),
   from_address VARCHAR(50),
   subject VARCHAR(50),
   message VARCHAR(500),
-  status VARCHAR(10)
+  status VARCHAR(50)
 )
