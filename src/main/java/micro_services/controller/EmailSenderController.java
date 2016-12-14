676b46c29ac7aa41c03f88e25f6a274433dcf6c8
eszterkaloz <eszterkaloz@gmail.com>
2016-12-14 11:38:54 +0100

    Added logging

--- src/main/java/micro_services/controller/EmailSenderController.java
+++ src/main/java/micro_services/controller/EmailSenderController.java
@@ -4,6 +4,8 @@ package micro_services.controller;
 import micro_services.dao.implementation.EmailDaoJDBC;
 import micro_services.model.Email;
 import micro_services.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.*;
 
 public class EmailSenderController {
@@ -12,18 +14,23 @@ public class EmailSenderController {
 
     private final EmailDaoJDBC emailDatabase = new EmailDaoJDBC();
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailSenderController.class);
+
     public EmailSenderController(EmailService emailService){
         this.emailService = emailService;
     }
 
     public String saveEmail(Request request, Response response) {
-         emailDatabase.add(
-                 new Email(request.queryParams("to"),
-                           request.queryParams("password"),
-                           request.queryParams("from"),
-                           request.queryParams("subject"),
-                           request.queryParams("message")
-        ));
+        logger.info("Adding emails to the database");
+
+        emailDatabase.add(
+                new Email(
+                    request.queryParams("to"),
+                    request.queryParams("password"),
+                    request.queryParams("from"),
+                    request.queryParams("subject"),
+                    request.queryParams("message")
+            ));
 
         return "Email saved to the database";
     }
