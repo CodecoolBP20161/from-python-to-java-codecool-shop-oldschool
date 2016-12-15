836efe80e9a39a7c6fc4af02826cfe1b400e7e9d
eszterkaloz <eszterkaloz@gmail.com>
2016-12-15 14:14:24 +0100

    getEmailService method changed

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -4,10 +4,14 @@ import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+import spark.Response;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OrderController {
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
@@ -18,15 +22,24 @@ public class OrderController {
     private static final String SUBJECT_PARAM_KEY = "subject";
     private static final String MESSAGE_PARAM_KEY = "message";
 
-    public static String getEmailService(String to, String password, String from, String subject, String message) throws IOException, URISyntaxException {
+    public static ModelAndView redirectToEmailService(spark.Request request, spark.Response response) throws IOException, URISyntaxException {
+        getEmailService();
+        Map params = new HashMap<>();
+        return new ModelAndView(params, "/payment");
+    }
+
+
+    public static String getEmailService() throws IOException, URISyntaxException {
         logger.info("Getting the EmailSenderService to send out necessary emails");
 
         URIBuilder builder = new URIBuilder(SERVICE_URL + "/email");
-        builder.addParameter(TO_ADDRESS_PARAM_KEY, to);
-        builder.addParameter(PASSWORD_PARAM_KEY, password);
-        builder.addParameter(FROM_ADDRESS_PARAM_KEY, from);
-        builder.addParameter(SUBJECT_PARAM_KEY, subject);
-        builder.addParameter(MESSAGE_PARAM_KEY, message);
+//        builder.addParameter(TO_ADDRESS_PARAM_KEY, to);
+//        builder.addParameter(PASSWORD_PARAM_KEY, password);
+//        builder.addParameter(FROM_ADDRESS_PARAM_KEY, from);
+//        builder.addParameter(SUBJECT_PARAM_KEY, subject);
+//        builder.addParameter(MESSAGE_PARAM_KEY, message);
+
+        logger.debug("URI for email sending: ", builder);
 
         return execute(builder.build());
     }
