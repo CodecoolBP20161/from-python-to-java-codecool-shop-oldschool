9f969f6fd070984c4d9c5d4cf46dbed1da6d93a2
eszterkaloz <eszterkaloz@gmail.com>
2017-01-24 17:58:05 +0100

    SController for shipping service implemented

---
+++ src/main/java/com/codecool/shop/controller/ShippingServiceController.java
@@ -0,0 +1,22 @@
+package com.codecool.shop.controller;
+
+
+import com.codecool.shop.model.ShippingCostCalculator;
+import com.codecool.shop.services.ShippingCostCalculatorService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class ShippingServiceController {
+    private ShippingCostCalculatorService shippingCostCalculatorService;
+    public static final String ORIGIN = "Budapest, Nagymező u. 44";
+
+    public ShippingServiceController(ShippingCostCalculatorService shippingCostCalculatorService) {
+        this.shippingCostCalculatorService = shippingCostCalculatorService;
+    }
+
+    public List<ShippingCostCalculator> getShippingCost(String destination) throws IOException, URISyntaxException{
+        return shippingCostCalculatorService.getShippingCost(destination, ORIGIN);
+    }
+}
