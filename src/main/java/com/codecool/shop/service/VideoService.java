8ab9c527a456ccd78f3748d7ed1679c5a6b42831
Kristof Galla <gallakristof@gmail.com>
2017-01-25 12:05:05 +0100

    Implement service layer for Video Service API

--- src/main/java/com/codecool/shop/service/VideoService.java
+++ src/main/java/com/codecool/shop/service/VideoService.java
@@ -1,7 +1,20 @@
 package com.codecool.shop.service;
 
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VideoService {
     public static VideoService INSTANCE;
+    private String API_URL = "https:0.0.0.0:60000/apivideos";
+    private String PARAM_KEY = "search";
 
     private VideoService() {
     }
@@ -12,5 +25,17 @@ public class VideoService {
         }
         return INSTANCE;
     }
-    // request video embed codes for the inserted 'product' argument
+
+    public List getEmbedCodes(String productName) throws URISyntaxException, IOException, JSONException {
+        List embedCodes = new ArrayList();
+        URIBuilder uriBuilder = new URIBuilder(API_URL);
+        uriBuilder.addParameter(PARAM_KEY, productName);
+        String response = Request.Get(uriBuilder.build()).execute().returnContent().asString();
+        JSONArray videos = new JSONArray(response);
+        for (int i = 0; i < videos.length(); i++) {
+            JSONObject video = (JSONObject) videos.get(i);
+            embedCodes.add(video.get("embed code").toString());
+        }
+        return embedCodes;
+    }
 }
