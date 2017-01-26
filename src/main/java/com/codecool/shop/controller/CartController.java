84d88bcd39f1862f39f76e48059d94ec2cff72b6
eszterkaloz <eszterkaloz@gmail.com>
2017-01-26 09:36:35 +0100

    Implemented shipping service and review page

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -7,31 +7,33 @@ import com.codecool.shop.dao.implementation.database.CustomerDaoJDBC;
 import com.codecool.shop.dao.implementation.database.LineItemDaoJDBC;
 import com.codecool.shop.dao.implementation.database.OrderDaoJDBC;
 import com.codecool.shop.model.*;
+import com.codecool.shop.services.ShippingCostCalculatorService;
+
 import org.apache.http.client.utils.URIBuilder;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CartController extends ShopController {
+    private static ShippingServiceController shippingServiceController = new ShippingServiceController(ShippingCostCalculatorService.getInstance());
+    private static String PAYMENT_SERVICE_URI = "http://localhost:9000/make-payment";
+    private static String EMAIL_SERVICE_URI = "http://localhost:8888/send-email/";
 
     public static ModelAndView renderOrder(Request req, Response res) {
-
-        // check if session contains order, instantiate if it doesn't
         CartInterface cart = req.session().attribute("order");
         if (cart == null) {
             cart = new Order();
             req.session().attribute("order", cart);
         }
 
-        // find product by product_id from request
         Product orderedProduct = productDataStore.find(Integer.parseInt(req.params(":product-id")));
-        // add lineitem to the order
         cart.addProduct(orderedProduct);
-        // redirect to the last path
         res.redirect(req.session().attribute("path"));
         return render(req, res);
     }
@@ -39,13 +41,11 @@ public class CartController extends ShopController {
     public static ModelAndView renderShoppingCart(Request req, Response res) {
         Map params = new HashMap<>();
 
-        // get items from cart
         if (req.session().attribute("order") != null) {
             Order order = req.session().attribute("order");
             params.put("total", order.getTotalPrice());
             params.put("lineitems", order.getLineItems());
             req.session().attribute("order_id", order.getId());
-            System.out.println("orderID = " + order.getId());
         }
 
         return new ModelAndView(params, "product/shopping_cart");
@@ -57,23 +57,63 @@ public class CartController extends ShopController {
         return new ModelAndView(params, "/checkout");
     }
 
-    public static ModelAndView saveCustomerDetails(Request req, Response res) throws URISyntaxException {
-        String PAYMENT_SERVICE_URI = "http://localhost:9000/make-payment";
-        String ORDER_SERVICE_URI = "http://localhost:8888/order/";
-
-        Map params = new HashMap<>();
+    public static ModelAndView saveCustomerDetails(Request req, Response res) throws IOException, URISyntaxException {
         OrderDao orderDao = new OrderDaoJDBC();
 
         Customer customer = setCustomer(req);
+        setOrder(req, orderDao, customer);
 
-        Order order = setOrder(req, orderDao, customer);
+        String destination = customer.getShippingAddress() + ", " +
+                            customer.getShippingCity() +  ", " +
+                            customer.getShippingZipcode() + " " +
+                            customer.getShippingCountry();
 
+        Map params = new HashMap<>();
+        params.put("shippingOptions", shippingServiceController.getShippingCost(destination));
+        params.put("destination", destination);
+        params.put("origin", shippingServiceController.ORIGIN);
+        return new ModelAndView(params, "shipping/shipping");
+    }
+
+    public static ModelAndView renderReviewPage(Request req, Response res) throws  URISyntaxException {
+        Map params = new HashMap<>();
+        ShippingCostCalculator chosenShippingOption = setShippingOption(req);
+        Order order = req.session().attribute("order");
+        int totalPrice = order.getTotalPrice() + chosenShippingOption.getCost();
+
+        params.put("paymentServiceUrl", paymentServiceURI(totalPrice, order));
+        params.put("option", chosenShippingOption);
+        params.put("total", totalPrice);
+        params.put("lineitems", order.getLineItems());
+
+        return new ModelAndView(params, "review");
+    }
+
+    private static String paymentServiceURI(int totalPrice, Order order) throws URISyntaxException {
         URIBuilder serviceURIBuilder = new URIBuilder(PAYMENT_SERVICE_URI);
-        serviceURIBuilder.addParameter("total", String.valueOf(order.getTotalPrice()));
-        serviceURIBuilder.addParameter("return-link", ORDER_SERVICE_URI + order.getId());
+        serviceURIBuilder.addParameter("total", String.valueOf(totalPrice));
+        serviceURIBuilder.addParameter("return-link", EMAIL_SERVICE_URI + order.getId());
+        return serviceURIBuilder.build().toString();
+    }
 
-        res.redirect(serviceURIBuilder.build().toASCIIString());
+    public static ModelAndView renderPayment(Request req, Response res) {
+        Map params = new HashMap<>();
+        OrderDao orderDataStore = new OrderDaoJDBC();
+        if (req.session().attribute("order_id") != null) {
+            orderDataStore.setOrderStatus(req.session().attribute("order_id"), OrderStatus.PAID);
+        }
+
         return new ModelAndView(params, "/payment");
+    }
+
+    private static ShippingCostCalculator setShippingOption (Request req) {
+        ShippingCostCalculator chosenOption = null;
+        for (ShippingCostCalculator option: ShippingCostCalculatorService.getInstance().getShippingOptions()) {
+            if(option.getName().equals(req.queryParams("shipping-option"))) {
+                chosenOption = option;
+            }
+        }
+        return chosenOption;
     }
 
     private static Order setOrder(Request req, OrderDao orderDao, Customer customer) {
@@ -106,16 +146,5 @@ public class CartController extends ShopController {
         customerDao.add(customer);
         return customer;
     }
-
-    public static ModelAndView renderPayment(Request req, Response res) {
-        Map params = new HashMap<>();
-        OrderDao orderDataStore = new OrderDaoJDBC();
-        if (req.session().attribute("order_id") != null) {
-            orderDataStore.setOrderStatus((Integer) req.session().attribute("order_id"), OrderStatus.PAID);
-        }
-
-        return new ModelAndView(params, "/payment");
-    }
-
 
 }
