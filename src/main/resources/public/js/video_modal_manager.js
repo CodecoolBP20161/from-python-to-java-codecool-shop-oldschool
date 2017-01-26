827118409526258926a99107b9df3c3fc79c9003
Kristof Galla <gallakristof@gmail.com>
2017-01-26 11:20:24 +0100

    Add AJAX request

--- src/main/resources/public/js/video_modal_manager.js
+++ src/main/resources/public/js/video_modal_manager.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-    /* Get iframe src attribute value i.e. YouTube video url
-     and store it in a variable */
-    var url = $("#productVideo").attr('src');
+    $('a[name="videoModal"]').click(function () {
+        var id = $(this).attr("id")
+        $.ajax({
+            url: '/video/' + id,
+            type: 'GET',
+            success: insertEmbedCode,
+            error: function (error) {
+                console.log(error);
+            }
 
-    /* Assign empty url value to the iframe src attribute when
-     modal hide, which stop the video playing */
-    $("#videoModal").on('hide.bs.modal', function () {
-        $("#productVideo").attr('src', '');
-    });
+        })
+        /* Get iframe src attribute value i.e. YouTube video url
+         and store it in a variable */
+        var url = $("#productVideo").attr('src');
 
-    /* Assign the initially stored url back to the iframe src
-     attribute when modal is displayed again */
-    $("#productVideo").on('show.bs.modal', function () {
-        $("#productVideo").attr('src', url);
+        /* Assign empty url value to the iframe src attribute when
+         modal hide, which stop the video playing */
+        $("#videoModal").on('hide.bs.modal', function () {
+            $("#productVideo").attr('src', '');
+        });
+
+        /* Assign the initially stored url back to the iframe src
+         attribute when modal is displayed again */
+        $("#productVideo").on('show.bs.modal', function () {
+            $("#productVideo").attr('src', url);
+        });
     });
-});
+})
+
+function insertEmbedCode(res) {
+    console.log(res)
+    $('.modal-body').html(res)
+}
