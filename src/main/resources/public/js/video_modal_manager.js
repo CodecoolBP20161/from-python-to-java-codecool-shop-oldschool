198ccb3db4f2f767a26c55cca74e14471736774a
Kristof Galla <gallakristof@gmail.com>
2017-01-26 11:33:20 +0100

    Show "Loading" while waiting for AJAX

--- src/main/resources/public/js/video_modal_manager.js
+++ src/main/resources/public/js/video_modal_manager.js
@@ -10,21 +10,14 @@ $(document).ready(function () {
             }
 
         })
-        /* Get iframe src attribute value i.e. YouTube video url
-         and store it in a variable */
-        var url = $("#productVideo").attr('src');
 
         /* Assign empty url value to the iframe src attribute when
          modal hide, which stop the video playing */
         $("#videoModal").on('hide.bs.modal', function () {
-            $("#productVideo").attr('src', '');
+            $('.modal-body').html("Loading")
         });
 
-        /* Assign the initially stored url back to the iframe src
-         attribute when modal is displayed again */
-        $("#productVideo").on('show.bs.modal', function () {
-            $("#productVideo").attr('src', url);
-        });
+
     });
 })
 
